4fe32b35920e30f27b19a61a1b6c332087a2c283
arslan990 <arslan@iiinigence.com>
2022-12-31 13:38:31 +0500

    initial commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "../layout";
+import { Dashboard } from "../containers";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path='/' element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
