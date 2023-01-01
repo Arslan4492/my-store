f6040da94637b3447676b91341ee747ee9d6b95c
Joudet Ahsan <joudetahsan@gmail.com>
2023-01-01 13:45:26 +0500

    changed the card styles

--- src/containers/details/Women.jsx
+++ src/containers/details/Women.jsx
@@ -10,7 +10,6 @@ import img6 from "../Details/images/img6.jpg";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { slice } from "../../store/Slice/Cart";
-import { Button } from "react-bootstrap";
 import Footer from "../../footer";
 
 function Women() {
@@ -90,40 +89,65 @@ function Women() {
           <Link to="/WomenDetails"> SEE CART</Link>
         </button>
       </h3>
-
       <div
-        style={{
-          display: "flex",
-          justifyContent: "spaced-evenly",
-          alignContent: "center",
-          flexDirection: "row",
-        }}
+        // style={{
+        //   display: "flex",
+        //   flexDirection: "row",
+        //   alignSelf: "center",
+        //   alignContent: "space-around",
+        //   justifyContent: "space-between",
+        //   width: "150%",
+        //   height: "100%",
+        //   flexBasis: "100%",
+        // }}
+        class="grid grid-cols-4"
       >
         {kapray.map((ele) => (
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" width="200px" src={ele.img} />
+          <div class="max-w-sm auto-rows-auto bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 grid  ">
+            <a href="#">
+              <img class="rounded-t-lg" src={ele.img} alt="" />
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {ele.name}
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {ele.code}
+              </p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Size: {ele.size}
+              </p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Color: {ele.color}
+              </p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Price: Rs. {ele.price}
+              </p>
 
-            <Card.Body>
-              <Card.Title style={{ textEmphasis: "ActiveBorder" }}>
-                {ele.name}
-              </Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item> {ele.code}</ListGroup.Item>
-              <ListGroup.Item>
-                Price: Rs
-                {ele.price}
-              </ListGroup.Item>
-              <ListGroup.Item>Size:{ele.size}</ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <Card href="#">Color:{ele.color}</Card>
-              <Card href="#">Gender:{ele.gender}</Card>
-            </Card.Body>
-            <button className="button2" onClick={() => handleClick(ele)}>
-              ADD TO CART
-            </button>
-          </Card>
+              <button
+                onClick={() => handleClick()}
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 float-center"
+              >
+                Add to Cart
+                <svg
+                  aria-hidden="true"
+                  class="w-4 h-4 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
         ))}
       </div>
       <Footer />
