995b1a3bccd6d75093a814e85a64eeeab9cc98cf
arslan990 <arslan@iiinigence.com>
2023-01-01 12:34:42 +0500

    tweak

--- src/layout/nav-bar/index.jsx
+++ src/layout/nav-bar/index.jsx
@@ -41,6 +41,15 @@ const NavBar = () => {
               <Link
                 class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
                 aria-current='page'
+                to='/men-collections'
+              >
+                Men
+              </Link>
+            </li>
+            <li>
+              <Link
+                class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+                aria-current='page'
                 to='/summer-collections'
               >
                 Summer collection
@@ -50,7 +59,7 @@ const NavBar = () => {
               <Link
                 class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
                 aria-current='page'
-                to='/winterCollection'
+                to='/winter-collections'
               >
                 Winter collection
               </Link>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,7 +8,7 @@ const AppRoutes = () => {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path='/' element={<Dashboard />} />
-          <Route path='/men' element={<Men />} />
+          <Route path='/men-collections' element={<Men />} />
           <Route path='/summer-collections' element={<SummerCollections />} />
           <Route path='/winter-Collections' element={<WinterCollections />} />
           <Route path='/cart' element={<Cart />} />
