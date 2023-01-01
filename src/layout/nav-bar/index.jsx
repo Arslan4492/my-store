e230914b1be560cc2ba70a3af78969710d7e130c
Muhammad Aqib Javed <aqibjaved5201@gmail.com>
2023-01-01 16:08:43 +0500

    winter-collection changed

--- src/layout/nav-bar/index.jsx
+++ src/layout/nav-bar/index.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
 const NavBar = () => {
   return (
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
-                to="/winterCollection"
-              >
-                Winter collection
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default"></div>
       </div>
     </nav>
   );
