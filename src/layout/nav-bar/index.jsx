3267ddb7ec03e1e73f702356d47d7bdfbb15a1ff
Muhammad Aqib Javed <aqibjaved5201@gmail.com>
2023-01-01 13:00:24 +0500

    winter collection

--- src/layout/nav-bar/index.jsx
+++ src/layout/nav-bar/index.jsx
@@ -16,15 +16,7 @@ const NavBar = () => {
                 Winter collection
               </Link>
             </li>
-            <li>
-              <Link
-                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
-                to="/cart"
-              >
-                Cart
-              </Link>
-            </li>
+      
           </ul>
         </div>
       </div>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "../layout";
 import { Dashboard } from "../containers";
-import Cart from "../containers/cart";
 import WinterCollection from "../containers/WinterCollection";
 
 const AppRoutes = () => {
@@ -11,7 +10,6 @@ const AppRoutes = () => {
         <Route element={<AppLayout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/winterCollection" element={<WinterCollection />} />
-          <Route path="/cart" element={<Cart />} />
         </Route>
       </Routes>
     </BrowserRouter>
