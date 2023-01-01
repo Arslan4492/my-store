e39d772897a243922fe0258b357fe1d719a73b07
Joudet Ahsan <joudetahsan@gmail.com>
2023-01-01 15:05:38 +0500

    final commit

--- src/containers/details/Women.jsx
+++ src/containers/details/Women.jsx
@@ -127,7 +127,7 @@ function Women() {
               </p>
 
               <button
-                onClick={() => handleClick()}
+                onClick={() => handleClick(ele)}
                 href="#"
                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 float-center"
               >
