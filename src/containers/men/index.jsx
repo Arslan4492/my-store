44f2363d7ecac9f32cfc6cd93c556e5ca7596a80
mansoorabbas123 <mansoor.abbas651@gmail.com>
2023-01-05 23:34:32 +0500

    zoom-in image css in men category/container

--- src/containers/men/index.jsx
+++ src/containers/men/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { getCategories } from "../../API"
 import { useQuery } from 'react-query'
+import './index.css';
 
 const Men = () => {
   const { isLoading, error, data:categories }  = useQuery('categories', getCategories)
@@ -19,27 +20,25 @@ const Men = () => {
   }
 
   return (
-    <>
-    {categoryMen.length>0 && ( <div className="w-full h-50px p-3">
+    <div className="w-full">
+    {categoryMen.length>0 && ( <div className="w-full image_container border-10 border-white hover:cursor-pointer">
         <img
           src={categoryMen[0].image}
           alt={`${categoryMen[0].title} image`}
-          className="w-full scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer"
         />
       </div>)}
 
-      <div className="flex flex-row flex-wrap">
+      <div className="flex flex-row flex-wrap justify-start">
      {categoryMen.length>0 && categoryMen[0]?.subCategories.map((category)=>
-     (<div className="w-1/2 h-450px p-3  border-2 border-white hover:cursor-pointer" key={category.id}>
+     (<div className="w-1/2 border-10 border-white hover:cursor-pointer image_container" key={category.id}>
           <img
             src={category.image}
             alt={`${category.title} image`}
-            className="w-full scale-100 hover:scale-105 ease-in duration-200"
           />
         </div>)
         ) }
       </div>
-    </>
+    </div>
   );
 };
 
