0f1bd06c05ca8938da82cc8d41979cff5e65c9a0
arslan990 <arslan@iiinigence.com>
2023-01-07 12:24:38 +0500

    merged with branch row-1

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,22 +1,20 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../footer";
+import Footer from "./footer";
 import Navbar from "./nav-bar";
 
 const AppLayout = () => {
   return (
-    <>
-      <div>
-        <div className='content-container h-full col-span-5'>
-          <Navbar />
-          <div className='flex flex-col p-3 m-0 w-full h-full'>
-            <div className='flex-fill'>
-              <Outlet />
-            </div>
+    <div>
+      <div className='content-container h-full col-span-5'>
+        <Navbar />
+        <div className='flex flex-col p-3 m-0 w-full h-full'>
+          <div className='flex-fill'>
+            <Outlet />
           </div>
         </div>
-        <Footer />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
