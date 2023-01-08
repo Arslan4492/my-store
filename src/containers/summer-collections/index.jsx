2dad1f8a160f770b84c76693eae207bf5245c298
Joudet Ahsan <joudetahsan@gmail.com>
2023-01-08 13:42:28 +0500

    not needed changes

--- src/containers/summer-collections/index.jsx
+++ src/containers/summer-collections/index.jsx
@@ -80,10 +80,6 @@ const SummerCollection = () => {
           } z-10 w-48 bg-white rounded shadow dark:bg-gray-700`}
         >
           <ul
-<<<<<<< HEAD
-            className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
-            aria-labelledby="dropdownBgHoverButton"
-=======
             style={{
               position: "absolute",
               background: "white",
@@ -92,7 +88,6 @@ const SummerCollection = () => {
             }}
             className='p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200'
             aria-labelledby='dropdownBgHoverButton'
->>>>>>> 91ae8f4ce958d537b4b2e9ec8b7ce1bc7c5de17b
           >
             <li>
               <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
@@ -148,106 +143,6 @@ const SummerCollection = () => {
           </ul>
         </div>
       </div>
-<<<<<<< HEAD
-      <div className="grid grid-cols-3 gap-5">
-        {isLoading || isFetching ? (
-          <p>Loading...</p>
-        ) : (
-          data?.products.map((product) => {
-            return categories.includes(product.category) ||
-              categories.length === 0 ? (
-              <div
-                key={product.id}
-                className="w-full shadow bg-white rounded-lg dark:bg-gray-800 dark:border-gray-700"
-              >
-                <img
-                  className="p-8 rounded-t-lg"
-                  src={product.images[0]}
-                  alt="product image"
-                />
-                <div className="px-5 pb-5">
-                  <a href="#">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                      {product.title}
-                    </h5>
-                  </a>
-                  <div className="flex items-center mt-2.5 mb-5">
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>First star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Second star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Third star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Fourth star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Fifth star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                      5.0
-                    </span>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                      $ {product.price}
-                    </span>
-                    <button
-                      onClick={() =>
-                        dispatch(cartSlice.actions.addToCart(product))
-                      }
-                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                      Add to cart
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ) : (
-              ""
-            );
-          })
-        )}
-      </div>
-=======
       {isLoading || isFetching ? (
         <p>Loading...</p>
       ) : (
@@ -293,7 +188,6 @@ const SummerCollection = () => {
           </div>
         </div>
       )}
->>>>>>> 91ae8f4ce958d537b4b2e9ec8b7ce1bc7c5de17b
     </>
   );
 };
